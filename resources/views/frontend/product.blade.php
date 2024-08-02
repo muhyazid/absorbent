87b6e47fa5bcbdbdb08311bdfa99310cbdf63fcc
andhikatok <awirasakti18@gmail.com>
2024-08-02 09:09:05 +0700

    css

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -17,7 +17,7 @@
     <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" />
     <link href="{{ asset('frontend/css/responsive.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('frontend/css/product.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('frontend/css/orderPopup.css') }}">
 </head>
 
 <body>
